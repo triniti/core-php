c352b002eeda781932071be2323db255f806759d
Eric Jacobson <contact@efjacobson.com>
2021-02-11 12:11:47 -0800

    boost

---
+++ src/Boost/Exception/TrinitiBoostException.php
@@ -0,0 +1,8 @@
+<?php
+declare(strict_types=1);
+
+namespace Triniti\Boost\Exception;
+
+interface TrinitiBoostException extends \Throwable
+{
+}
---
+++ src/Boost/SponsorAggregate.php
@@ -0,0 +1,208 @@
+<?php
+declare(strict_types=1);
+
+namespace Triniti\Boost;
+
+use Gdbots\Ncr\Aggregate;
+use Gdbots\Pbj\Message;
+use Gdbots\Pbj\MessageResolver;
+
+class SponsorAggregate extends Aggregate
+{
+    /**
+     * This is for legacy uses of command/event mixins for common
+     * ncr operations. It will be removed in 3.x.
+     *
+     * @param string $name
+     * @param array  $arguments
+     *
+     * @return mixed
+     */
+    public function __call(string $name, array $arguments)
+    {
+        $newName = str_replace('Sponsor', 'Node', $name);
+        if ($newName !== $name && is_callable([$this, $newName])) {
+            return $this->$newName(...$arguments);
+        }
+    }
+
+    /**
+     * This is for legacy uses of command/event mixins for common
+     * ncr operations. It will be removed in 3.x.
+     *
+     * @param Message $command
+     *
+     * @return Message
+     *
+     * @deprecated Will be removed in 3.x.
+     */
+    protected function createNodeCreatedEvent(Message $command): Message
+    {
+        return MessageResolver::resolveCurie('*:boost:event:sponsor-created:v1')::create();
+    }
+
+    /**
+     * This is for legacy uses of command/event mixins for common
+     * ncr operations. It will be removed in 3.x.
+     *
+     * @param Message $command
+     *
+     * @return Message
+     *
+     * @deprecated Will be removed in 3.x.
+     */
+    protected function createNodeDeletedEvent(Message $command): Message
+    {
+        return MessageResolver::resolveCurie('*:boost:event:sponsor-deleted:v1')::create();
+    }
+
+    /**
+     * This is for legacy uses of command/event mixins for common
+     * ncr operations. It will be removed in 3.x.
+     *
+     * @param Message $command
+     *
+     * @return Message
+     *
+     * @deprecated Will be removed in 3.x.
+     */
+    protected function createNodeExpiredEvent(Message $command): Message
+    {
+        return MessageResolver::resolveCurie('*:boost:event:sponsor-expired:v1')::create();
+    }
+
+    /**
+     * This is for legacy uses of command/event mixins for common
+     * ncr operations. It will be removed in 3.x.
+     *
+     * @param Message $command
+     *
+     * @return Message
+     *
+     * @deprecated Will be removed in 3.x.
+     */
+    protected function createNodeLockedEvent(Message $command): Message
+    {
+        return MessageResolver::resolveCurie('*:boost:event:sponsor-locked:v1')::create();
+    }
+
+    /**
+     * This is for legacy uses of command/event mixins for common
+     * ncr operations. It will be removed in 3.x.
+     *
+     * @param Message $command
+     *
+     * @return Message
+     *
+     * @deprecated Will be removed in 3.x.
+     */
+    protected function createNodeMarkedAsDraftEvent(Message $command): Message
+    {
+        return MessageResolver::resolveCurie('*:boost:event:sponsor-marked-as-draft:v1')::create();
+    }
+
+    /**
+     * This is for legacy uses of command/event mixins for common
+     * ncr operations. It will be removed in 3.x.
+     *
+     * @param Message $command
+     *
+     * @return Message
+     *
+     * @deprecated Will be removed in 3.x.
+     */
+    protected function createNodeMarkedAsPendingEvent(Message $command): Message
+    {
+        return MessageResolver::resolveCurie('*:boost:event:sponsor-marked-as-pending:v1')::create();
+    }
+
+    /**
+     * This is for legacy uses of command/event mixins for common
+     * ncr operations. It will be removed in 3.x.
+     *
+     * @param Message $command
+     *
+     * @return Message
+     *
+     * @deprecated Will be removed in 3.x.
+     */
+    protected function createNodePublishedEvent(Message $command): Message
+    {
+        return MessageResolver::resolveCurie('*:boost:event:sponsor-published:v1')::create();
+    }
+
+    /**
+     * This is for legacy uses of command/event mixins for common
+     * ncr operations. It will be removed in 3.x.
+     *
+     * @param Message $command
+     *
+     * @return Message
+     *
+     * @deprecated Will be removed in 3.x.
+     */
+    protected function createNodeRenamedEvent(Message $command): Message
+    {
+        return MessageResolver::resolveCurie('*:boost:event:sponsor-renamed:v1')::create();
+    }
+
+    /**
+     * This is for legacy uses of command/event mixins for common
+     * ncr operations. It will be removed in 3.x.
+     *
+     * @param Message $command
+     *
+     * @return Message
+     *
+     * @deprecated Will be removed in 3.x.
+     */
+    protected function createNodeScheduledEvent(Message $command): Message
+    {
+        return MessageResolver::resolveCurie('*:boost:event:sponsor-scheduled:v1')::create();
+    }
+
+    /**
+     * This is for legacy uses of command/event mixins for common
+     * ncr operations. It will be removed in 3.x.
+     *
+     * @param Message $command
+     *
+     * @return Message
+     *
+     * @deprecated Will be removed in 3.x.
+     */
+    protected function createNodeUnlockedEvent(Message $command): Message
+    {
+        return MessageResolver::resolveCurie('*:boost:event:sponsor-unlocked:v1')::create();
+    }
+
+    /**
+     * This is for legacy uses of command/event mixins for common
+     * ncr operations. It will be removed in 3.x.
+     *
+     * @param Message $command
+     *
+     * @return Message
+     *
+     * @deprecated Will be removed in 3.x.
+     */
+    protected function createNodeUnpublishedEvent(Message $command): Message
+    {
+        return MessageResolver::resolveCurie('*:boost:event:sponsor-unpublished:v1')::create();
+    }
+
+    /**
+     * This is for legacy uses of command/event mixins for common
+     * ncr operations. It will be removed in 3.x.
+     *
+     * @param Message $command
+     *
+     * @return Message
+     *
+     * @deprecated Will be removed in 3.x.
+     */
+    protected function createNodeUpdatedEvent(Message $command): Message
+    {
+        return MessageResolver::resolveCurie('*:boost:event:sponsor-updated:v1')::create();
+    }
+}
