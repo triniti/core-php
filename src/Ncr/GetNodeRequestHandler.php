1f35860af63613ddedd3be7b9b56315dd0d46c5a
Greg Brown <greg@12dnetworks.com>
2021-03-11 19:37:48 -0800

    add legacy GetNodeRequestHandler

---
+++ src/Ncr/GetNodeRequestHandler.php
@@ -0,0 +1,35 @@
+<?php
+declare(strict_types=1);
+
+namespace Triniti\Ncr;
+
+use Gdbots\Ncr\GetNodeRequestHandler as BaseGetNodeRequestHandler;
+use Gdbots\Pbj\Message;
+use Gdbots\Pbj\MessageResolver;
+use Gdbots\Pbjx\Pbjx;
+
+/**
+ * @deprecated will be removed in 3.x
+ */
+class GetNodeRequestHandler extends BaseGetNodeRequestHandler
+{
+    public static function handlesCuries(): array
+    {
+        $vendor = MessageResolver::getDefaultVendor();
+        $curies = array_flip(MessageResolver::findAllUsingMixin('gdbots:ncr:mixin:get-node-request:v1', false));
+
+        unset($curies['gdbots:ncr:request:get-node-request']);
+        unset($curies['gdbots:iam:request:get-user-request']);
+        unset($curies["{$vendor}:iam:request:get-user-request"]);
+        unset($curies['triniti:sys:request:get-redirect-request']);
+        unset($curies["{$vendor}:sys:request:get-redirect-request"]);
+
+        return array_keys($curies);
+    }
+
+    protected function createGetNodeResponse(Message $request, Pbjx $pbjx): Message
+    {
+        $curie = str_replace('-request', '-response', $request::schema()->getCurie()->toString());
+        return MessageResolver::resolveCurie($curie)::create();
+    }
+}
