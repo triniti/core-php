1138c496dba635a6f9c04db4af9dbe88bca5bd41
efjacobson <efjacobson@users.noreply.github.com>
2021-06-03 13:47:41 -0700

    v1.0.5
    * Subscribe `NcrArticleProjector` to `apple-news-article-synced` mixin and event.

--- src/News/NcrArticleProjector.php
+++ src/News/NcrArticleProjector.php
@@ -10,8 +10,10 @@ class NcrArticleProjector extends NcrProjector
     public static function getSubscribedEvents()
     {
         return [
-            'triniti:news:mixin:article-slotting-removed' => 'onNodeEvent',
-            'triniti:news:event:article-slotting-removed' => 'onNodeEvent',
+            'triniti:news:mixin:article-slotting-removed'  => 'onNodeEvent',
+            'triniti:news:event:article-slotting-removed'  => 'onNodeEvent',
+            'triniti:news:mixin:apple-news-article-synced' => 'onNodeEvent',
+            'triniti:news:event:apple-news-article-synced' => 'onNodeEvent',
         ];
     }
 }
