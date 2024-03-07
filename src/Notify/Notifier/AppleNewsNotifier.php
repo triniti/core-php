7c60dfa25070cf9c281f36a2ea29304fcf3661a1
Teebken <trenttee1020@gmail.com>
2024-03-07 16:50:24 -0700

    remove other uses of string casting

--- src/Notify/Notifier/AppleNewsNotifier.php
+++ src/Notify/Notifier/AppleNewsNotifier.php
@@ -144,7 +144,7 @@ class AppleNewsNotifier implements Notifier
         $document = $this->marshaler->marshal($article);
         $metadata = $this->createArticleMetadata($article);
         $metadata['revision'] = $article->get('apple_news_revision');
-        $result = $this->api->updateArticle((string)$article->get('apple_news_id'), $document, $metadata);
+        $result = $this->api->updateArticle($article->fget('apple_news_id'), $document, $metadata);
 
         if ($result['ok']) {
             return $result;
@@ -158,7 +158,7 @@ class AppleNewsNotifier implements Notifier
         $latestRevision = $this->getLatestRevision($notification);
         if (null !== $latestRevision && $metadata['revision'] !== $latestRevision) {
             $metadata['revision'] = $latestRevision;
-            $result = $this->api->updateArticle((string)$article->get('apple_news_id'), $document, $metadata);
+            $result = $this->api->updateArticle($article->fget('apple_news_id'), $document, $metadata);
         }
 
         if ($result['ok']) {
