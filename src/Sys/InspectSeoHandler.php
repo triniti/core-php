ad00d88a7e893392ede77020c0a5cff138f09e04
Donte Burney <digitalartcrew@gmail.com>
2024-04-04 11:50:21 -1000

    clean up

--- src/Sys/InspectSeoHandler.php
+++ src/Sys/InspectSeoHandler.php
@@ -7,7 +7,6 @@ use Defuse\Crypto\Crypto;
 use Defuse\Crypto\Key;
 use Gdbots\Ncr\Ncr;
 use Gdbots\Pbj\Message;
-use Gdbots\Pbj\MessageResolver;
 use Gdbots\Pbjx\CommandHandler;
 use Gdbots\Pbjx\Pbjx;
 use Gdbots\UriTemplate\UriTemplateService;
@@ -16,10 +15,7 @@ use Google\Service\SearchConsole\InspectUrlIndexResponse;
 
 class InspectSeoHandler implements CommandHandler
 {
-//    private $retryCount;
-//    // make max tries a flag
-//    private $maxRetries;
-    private String $siteUrl;
+    private string $siteUrl;
     private Ncr $ncr;
     private Key $key;
 
@@ -30,16 +26,14 @@ class InspectSeoHandler implements CommandHandler
         ];
     }
 
-    public function __construct(Ncr $ncr, string $siteUrl,
-// int $maxRetries
-Key $key
+    public function __construct(Ncr $ncr, Key $key, string $siteUrl
+
     )
     {
         $this->ncr = $ncr;
         $this->siteUrl = $siteUrl;
         $this->key = $key;
-//        $this->maxRetries = $maxRetries;
-//        $this->retryCount = 0;
+
     }
 
     public function handleCommand(Message $command, Pbjx $pbjx): void
@@ -58,12 +52,11 @@ Key $key
 
     public function checkIndexStatus(Message $node): void {
         $successStates = ['INDEXING_ALLOWED', 'SUCCESSFUL'];
+        $ampVerdict = null;
 
         $url = UriTemplateService::expand(
             "{$node::schema()->getQName()}.canonical", $node->getUriTemplateVars()
         );
-
-        $urlStatus = null;
 
         try {
             $urlStatus = $this->getUrlIndexResponse($url);
@@ -72,39 +65,44 @@ Key $key
            return;
         }
 
-//        $webVerdict = $urlStatus->get('inspectionResult')->get('indexStatusResult')->get('verdict');
-//        $indexingState = $urlStatus->get('inspectionResult')->get('indexStatusResult')->get('indexingState');
-//        $ampVerdict = $urlStatus->get('inspectionResult')->get('ampResult')->get('verdict');
+        $webVerdict = $urlStatus->getInspectionResult()->getIndexStatusResult()->getVerdict();
 
-//        if ($node->get('is_unlisted') === 'true' || $node->get('amp_enabled') === "false"){
-//            if ($webVerdict === 'PASS') {
-//                error_log("Page should not be indexed");
-//            }
-//        }
-//
-//        if ($node->get('amp_enabled') === "true"){
-//            if ($webVerdict === 'PASS' && $ampVerdict === 'PASS' && in_array($indexingState, $successStates)) {
-//                $this->handleIndexingSuccess();
-//            }
-//        }
-//
-//        if ($webVerdict === 'PASS' && in_array($indexingState, $successStates)) {
-//            $this->handleIndexingSuccess();
-//        }
-//
-//        $this->handleIndexingFailure();
+        if ($urlStatus->getInspectionResult() !== null &&
+            $urlStatus->getInspectionResult()->getAmpResult() !== null) {
+            $ampVerdict = $urlStatus->getInspectionResult()->getAmpResult()->getVerdict();
+        }
+
+        $indexingState = $urlStatus->getInspectionResult()->getIndexStatusResult()->getIndexingState();
+
+        if ($node->get('is_unlisted') === 'true' || $node->get('amp_enabled') === "false"){
+            if ($webVerdict === 'PASS') {
+                error_log("Page should not be indexed");
+            }
+        }
+
+        if ($node->get('amp_enabled') === "true"){
+            if ($webVerdict === 'PASS' && $ampVerdict === 'PASS' && in_array($indexingState, $successStates)) {
+                $this->handleIndexingSuccess();
+            }
+        }
+
+        if ($webVerdict === 'PASS' && in_array($indexingState, $successStates)) {
+            $this->handleIndexingSuccess();
+        }
+
+        $this->handleIndexingFailure();
     }
 
     public function getUrlIndexResponse(String $url): InspectUrlIndexResponse {
         $request = new \Google_Service_SearchConsole_InspectUrlIndexRequest();
-        $request->setSiteUrl('sc-domain:tmz.com');
+        $request->setSiteUrl($this->siteUrl);
         $request->setInspectionUrl($url);
         $client = new \Google_Client();
         $client->setAuthConfig(json_decode(base64_decode(Crypto::decrypt(getenv('GOOGLE_SEARCH_CONSOLE_API_SERVICE_ACCOUNT_OAUTH_CONFIG'), $this->key)), true));
         $client->addScope(\Google_Service_SearchConsole::WEBMASTERS_READONLY);
         $service = new \Google_Service_SearchConsole($client);
 
-        return dump($service->urlInspection_index->inspect($request));
+        return $service->urlInspection_index->inspect($request);
     }
 
     public function handleIndexingSuccess(): void {}
