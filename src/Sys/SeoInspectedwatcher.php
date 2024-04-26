e76bc48fead1ff7e772ac7229ce4b8bc379b464c
Donte Burney <digitalartcrew@gmail.com>
2024-04-25 14:01:16 -1000

    added seo watcher

---
+++ src/Sys/SeoInspectedwatcher.php
@@ -0,0 +1,19 @@
+<?php
+declare(strict_types=1);
+
+namespace Triniti\News;
+
+use Gdbots\Ncr\Event\NodeProjectedEvent;
+use Gdbots\Pbjx\EventSubscriber;
+
+class SeoInspectedWatcher implements EventSubscriber
+{
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            'triniti:sys:event:seo-inspected' => 'onSeoInspected',
+        ];
+    }
+
+    public function onSeoInspected(NodeProjectedEvent $pbjxEvent): void {}
+}
