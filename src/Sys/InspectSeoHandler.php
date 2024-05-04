e5b26554fd93be642d211addda822d61e1be9b4d
Donte Burney <digitalartcrew@gmail.com>
2024-05-03 23:05:05 -1000

    use triniti class

--- src/Sys/InspectSeoHandler.php
+++ src/Sys/InspectSeoHandler.php
@@ -16,7 +16,6 @@ use Google\Service\SearchConsole\InspectUrlIndexResponse;
 use Psr\Log\LoggerInterface;
 use Psr\Log\NullLogger;
 use Triniti\Schemas\Sys\Event\SeoInspectedV1;
-use Triniti\Sys\SeoInspectedWatcher;
 
 
 class InspectSeoHandler implements CommandHandler
