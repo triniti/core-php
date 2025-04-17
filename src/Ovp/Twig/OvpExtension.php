c393022a32516ad84445f98c0c738db4692f9081
Donte Burney <donte.burney@fox.com>
2025-04-17 11:30:15 -1000

    updated

--- src/Ovp/Twig/OvpExtension.php
+++ src/Ovp/Twig/OvpExtension.php
@@ -5,21 +5,14 @@ namespace Triniti\Ovp\Twig;
 
 use Gdbots\Pbj\Message;
 use Gdbots\Pbj\WellKnown\NodeRef;
-use Triniti\Ovp\ArtifactUrlProvider;
 use Triniti\Ovp\Util\OvpUtil;
 use Triniti\Schemas\Dam\AssetId;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFunction;
 
+
 final class OvpExtension extends AbstractExtension
 {
-    private ArtifactUrlProvider $artifactUrlProvider;
-
-    public function __construct(ArtifactUrlProvider $artifactUrlProvider)
-    {
-        $this->artifactUrlProvider = $artifactUrlProvider;
-    }
-
     public function getFunctions(): array
     {
         return [
