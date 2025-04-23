239c1f82c7995e8556a70f0917955621aec65c4e
Donte Burney <donte.burney@fox.com>
2025-04-23 13:37:08 -1000

    updates

--- src/Ovp/Twig/OvpExtension.php
+++ src/Ovp/Twig/OvpExtension.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 namespace Triniti\Ovp\Twig;
 
 use Triniti\Ovp\ArtifactUrlProvider;
-use Triniti\Ovp\Util\OvpUtil;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFunction;
 
